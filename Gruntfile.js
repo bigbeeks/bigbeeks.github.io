f7189042a6288a5198e626640dd19b141ab0a7d4
Trever Santora <trever@iron-point.com>
2015-07-17 22:55:02 -0700

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,9 +119,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', [
     'sass',
-    'uglify',
-    // 'penthouse'
-    //'uncss'
+    'uglify'
   ]);
   grunt.registerTask('default', [
     'shell',
